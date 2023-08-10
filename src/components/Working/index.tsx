29ccccd05421dce416335956e03cd97b707b04c9
thanhbui27 <bthanh2001@gmail.com>
2023-08-10 14:28:55 +0700

    add component working

---
+++ src/components/Working/index.tsx
@@ -0,0 +1,13 @@
+import WorkingBest from "./components/WorkingBest";
+import WorkingProcess from "./components/WorkingProcess";
+
+const Working = () => {
+  return (
+    <div className="working">
+      <WorkingBest />
+      <WorkingProcess />
+    </div>
+  );
+};
+
+export default Working;
